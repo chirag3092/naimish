ca8df1f6858d89d30d70975f7241334df621d33b
Chirag Patel <PEOPLE\chiragp@lp8379.people.local>
2021-05-22 17:33:35 +0530

    check workflow

---
+++ src/components/MySelf/MyDetails/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MyDetails = () => <div>My Details</div>;
+
+export default MyDetails;
---
+++ src/components/MySelf/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MyDetails from './MyDetails';
+import MyInfo from './MyInfo';
+
+const MySelf = () => {
+  return (
+    <>
+      <MyInfo />
+      <MyDetails />
+    </>
+  );
+};
+
+export default MySelf;
