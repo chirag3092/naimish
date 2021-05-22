d68386af658ec28ddc216b089a9c0e17d2dbac2a
Chirag Patel <PEOPLE\chiragp@lp8379.people.local>
2021-05-22 22:04:21 +0530

    add details

--- src/components/MySelf/MyInfo/index.tsx
+++ src/components/MySelf/MyInfo/index.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-const MyInfo = () => <div>My Info Here and add more details later.
-
-
-  
-</div>;
+const MyInfo = () => <div>My Info Here and add more details later.</div>;
 
 export default MyInfo;
